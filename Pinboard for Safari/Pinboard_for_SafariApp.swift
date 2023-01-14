78585a015bc070e38e89177b1bd4cf92004362e8
Simon Whitaker <sw@netcetera.org>
2023-01-14 09:54:44 +0000

    Initial Commit

---
+++ Pinboard for Safari/Pinboard_for_SafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinboard_for_SafariApp.swift
+//  Pinboard for Safari
+//
+//  Created by Simon Whitaker on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pinboard_for_SafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
